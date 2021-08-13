8d2b2d746416e3f4ae0daba0f23fe614c7f29726
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-08-13 21:00:10 +0100

    Updated

--- src/js/modules/mediaQueries.js
+++ src/js/modules/mediaQueries.js
@@ -1,12 +1,16 @@
 let nav = document.querySelector("nav"),
   navList = document.querySelector(".nav__list"),
-  navToggle = document.querySelector(".nav__toggle");
+  navToggle = document.querySelector(".nav__toggle"),
+  navItems = document.querySelectorAll(".nav__item");
 
 const arrays = [nav, navList, navToggle];
 
 navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
   navList.classList.toggle("active");
+  navItems.forEach((navitem) => {
+    navitem.classList.toggle("active");
+  });
 });
 
 const activateMobileView = () => {
